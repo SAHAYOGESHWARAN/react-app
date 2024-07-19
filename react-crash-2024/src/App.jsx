1ff011b927b943c004d628f00ae754dd79b88820
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-07-19 09:23:12 +0530

    add reacr app master

---
+++ react-crash-2024/src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+  const name = 'saha';
+  const x = 10;
+  const y = 20;
+  return (
+  <>
+    <div className='text-5xl'>App</div>
+  <p>hello {name}</p>
+  </>
+  );
+  };
+export default App; 
+ 
